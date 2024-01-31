8f40f9e68ffbf74e0538c31e5da28c4871aa76ab
nellaibill <nellaibill@gmail.com>
2024-01-31 22:28:59 +0800

    Build-5.0(5.1) and  loginVC.swift file changes(yoonus)

--- BeauteSoft/AppView/Login/LoginVC.swift
+++ BeauteSoft/AppView/Login/LoginVC.swift
@@ -30,7 +30,7 @@ class LoginVC: UIViewController {
             txtPassword.isSecureTextEntry =  true
         }
     }
-    @IBAction func actionLogin(_ sender: UIButton) {
+@IBAction func actionLogin(_ sender: UIButton) {
         if txtUsername.isBlank {
             self.singleButtonAlertWith(message: Messages.emailPlease)
         } else if txtPassword.text!.isEmpty {
@@ -56,11 +56,13 @@ class LoginVC: UIViewController {
                 base = "http://originida.ddns.net:8686/main/api/"
                 baseDashboard = "http://originida.ddns.net:8686/main/api/"}
             else if (txtLocale.trimmedText.uppercased() == "TNC"){
+                base = "http://103.253.14.203:4001/main_api/"
+                baseDashboard = "http://103.253.14.203:4001/main_api/"}
+            else if (txtLocale.trimmedText.uppercased() == "TNCOLD"){
                 base = "http://103.253.15.102:88/cloudtncapi/api/"
                 baseDashboard = "http://103.253.15.102:88/cloudtncapi/api"}
-            else if (txtLocale.trimmedText.uppercased() == "TNCOLD"){
-                base = "http://tncbeautysg.ddns.net:88/main/api/"
-                baseDashboard = "http://tncbeautysg.ddns.net:88/main/api/"}
+                //base = "http://tncbeautysg.ddns.net:88/main/api/"
+                //baseDashboard = "http://tncbeautysg.ddns.net:88/main/api/"}
             else if (txtLocale.trimmedText.uppercased() == "FOREVER"){
                 base = "http://beautyforeverhq.ddns.net:88/main/api/"
                 baseDashboard = "http://beautyforeverhq.ddns.net:88/main/api/"}
@@ -77,11 +79,13 @@ class LoginVC: UIViewController {
                 base = "http://103.253.15.102/demo/api/"
                 baseDashboard = "http://103.253.15.102/demo/api/"}
             else if (txtLocale.trimmedText.uppercased() == "SKINCLOUD"){
+                base = "http://103.253.14.203:4002/main_api/"
+                baseDashboard = "http://103.253.14.203:4002/main_api/"}
+            else if (txtLocale.trimmedText.uppercased() == "SKINCLOUDOLD"){
                 base = "http://sequoiasg.ddns.net:7010/main_api/api/"
                 baseDashboard = "http://sequoiasg.ddns.net:7010/main_api/api/"}
-            else if (txtLocale.trimmedText.uppercased() == "SKINCLOUDOLD"){
-                base = "http://103.253.15.73:8489/Main_API/api/"
-                baseDashboard = "http://103.253.15.73:8489/Main_API/api/"}
+                //base = "http://103.253.15.73:8489/Main_API/api/"
+                //baseDashboard = "http://103.253.15.73:8489/Main_API/api/"}
             else if (txtLocale.trimmedText.uppercased() == "ESTH"){
                 base = "http://103.253.15.102:9399/Main_API/api/"
                 baseDashboard = "http://103.253.15.102:9399/Main_API/api/"}
@@ -95,8 +99,8 @@ class LoginVC: UIViewController {
                 base = "http://sequoiasg.ddns.net:7014/mainapi/api/"
                 baseDashboard = "http://sequoiasg.ddns.net:7014/mainapi/api/"}
             else if (txtLocale.trimmedText.uppercased() == "MARMA"){
-                base = "https://crm.soha.edu.sg/apimarmaclinic/api/"
-                baseDashboard = "https://crm.soha.edu.sg/apimarmaclinic/api/"}
+                base = "http://103.253.14.203:4003/Main_API/"
+                baseDashboard = "http://103.253.14.203:4003/Main_API/"}
             else if (txtLocale.trimmedText.uppercased() == "ELVA"){
                 base = "http://103.253.14.203:4004/Main_API/api/"
                 baseDashboard = "http://103.253.14.203:4004/Main_API/api/"}
@@ -115,6 +119,51 @@ class LoginVC: UIViewController {
             else if (txtLocale.trimmedText.uppercased() == "IMPRESSION"){
                 base = "http://103.253.15.102:9591/Main_API/api/"
                 baseDashboard = "http://103.253.15.102:9591/Main_API/api/"}
+            else if (txtLocale.trimmedText.uppercased() == "EMBRACE"){
+                base = "http://103.253.14.203:4015/Main_API/"
+                baseDashboard = "http://103.253.14.203:4015/Main_API/"}
+            else if (txtLocale.trimmedText.uppercased() == "MYDEMO"){
+                base = "http://103.253.14.203:4029/mainapi/"
+                baseDashboard = "http://103.253.14.203:4029/mainapi/"}
+            else if (txtLocale.trimmedText.uppercased() == "NAILQUEEN"){
+                base = "http://103.253.14.203:4012/Main_API/"
+                baseDashboard = "http://103.253.14.203:4012/Main_API/"}
+            else if (txtLocale.trimmedText.uppercased() == "SINCERESKIN"){
+                base = "http://103.253.15.102:9598/Main_API/"
+                baseDashboard = "http://103.253.15.102:9598/Main_API/"}
+            else if (txtLocale.trimmedText.uppercased() == "SLBEAUTY"){
+                base = "http://103.253.15.102:9595/Main_API/"
+                baseDashboard = "http://103.253.15.102:9595/Main_API/"}
+            else if (txtLocale.trimmedText.uppercased() == "ESTHETIKA"){
+                base = "http://103.253.15.102:9399/Main_API/api/"
+                baseDashboard = "http://103.253.15.102:9399/Main_API/api/"}
+            else if (txtLocale.trimmedText.uppercased() == "ESTHETICA"){
+                base = "http://103.253.15.102:9399/Main_API/api/"
+                baseDashboard = "http://103.253.15.102:9399/Main_API/api/"}
+            else if (txtLocale.trimmedText.uppercased() == "KIREI"){
+                base = "http://103.253.15.102:9596/Main_API/"
+                baseDashboard = "http://103.253.15.102:9596/Main_API/"}
+            else if (txtLocale.trimmedText.uppercased() == "LUXELLE"){
+                base = "http://103.253.14.203:4030/Main_API/"
+                baseDashboard = "http://103.253.14.203:4030/Main_API/"}
+            else if (txtLocale.trimmedText.uppercased() == "FOREVER"){
+                base = "http://103.253.14.203:4031/Main_API/"
+                baseDashboard = "http://103.253.14.203:4031/Main_API/"}
+            else if (txtLocale.trimmedText.uppercased() == "LOYALSPA"){
+                base = "http://103.253.14.203:4042/Main_API/"
+                baseDashboard = "http://103.253.14.203:4042/Main_API/"}
+            else if (txtLocale.trimmedText.uppercased() == "MONARCH"){
+                base = "http://103.253.14.203:4043/Main_API/"
+                baseDashboard = "http://103.253.14.203:4043/Main_API/"}
+            else if (txtLocale.trimmedText.uppercased() == "ZENTH"){
+                base = "http://103.253.14.203:4041/Main_API/"
+                baseDashboard = "http://103.253.14.203:4041/Main_API/"}
+            else if (txtLocale.trimmedText.uppercased() == "DVCBORNEO"){
+                base = "http://103.253.14.203:4046/Main_API/"
+                baseDashboard = "http://103.253.14.203:4046/Main_API/"}
+            else if (txtLocale.trimmedText.uppercased() == "SCENEFIS"){
+                base = "http://103.253.15.75:7049/Main_API/"
+                baseDashboard = "http://103.253.15.75:7049/Main_API/"}
             else {
                 base = "http://103.253.14.203/main/api/"
                 baseDashboard = "http://103.253.14.203/main/api/"}
@@ -124,8 +173,8 @@ class LoginVC: UIViewController {
             //
              login()
         }
-       
-    }
+        
+}
     
     @IBAction func actionForgotpassword(_ sender: UIButton) {
         let forgotPasswordVC = ForgotPasswordVC.instantiateFrom(storyboard: .prelogin)
